e97ae0608f85b72cf819351bbdca71ba2386b6e7
t2minht <tuongtran0240@gmail.com>
2024-09-06 16:26:16 -0500

    imported all of hte smash chracters images

---
+++ src/app/SSBUPicker/page.js
@@ -0,0 +1,96 @@
+import { MantineProvider } from '@mantine/core';
+import Image from 'next/image';
+import Mario from '/public/assets/characters/Mario.png';
+import DrMario from '/public/assets/characters/DrMario.png';
+import DonkeyKong from '/public/assets/characters/DonkeyKong.png';
+import DiddyKing from '/public/assets/characters/DiddyKong.png';
+import Link from '/public/assets/characters/Link.png';
+import Zelda from '/public/assets/characters/Zelda.png';
+import ZSS from '/public/assets/characters/ZSS.png';
+import Sheik from '/public/assets/characters/Sheik.png';
+import Ganondorf from '/public/assets/characters/Ganondorf.png';
+import YoungLink from '/public/assets/characters/YoungLink.png';
+import ToonLink from '/public/assets/characters/ToonLink.png';
+import Villager from '/public/assets/characters/Villager.png';
+import Samus from '/public/assets/characters/Samus.png';
+import DarkSamus from '/public/assets/characters/DarkSamus.png';
+import Ridley from '/public/assets/characters/Ridley.png';
+import Yoshi from '/public/assets/characters/Yoshi.png';
+import Kirby from '/public/assets/characters/Kirby.png';
+import KingDedede from '/public/assets/characters/KingDedede.png';
+import MetaKnight from '/public/assets/characters/MetaKnight.png';
+import Fox from '/public/assets/characters/Fox.png';
+import Falco from '/public/assets/characters/Falco.png';
+import Wolf from '/public/assets/characters/Wolf.png';
+import Pikachu from '/public/assets/characters/Pikachu.png';
+import Pichu from '/public/assets/characters/Pichu.png';
+import PiranhaPlant from '/public/assets/characters/PiranhaPlant.png';
+import Jigglypuff from '/public/assets/characters/Jigglypuff.png';
+import Ness from '/public/assets/characters/Ness.png';
+import Lucas from '/public/assets/characters/Lucas.png';
+import Ike from '/public/assets/characters/Ike.png';
+import CaptainFalcon from '/public/assets/characters/CaptainFalcon.png';
+import IceClimbers from '/public/assets/characters/IceClimbers.png';
+import PokemonTrainer from '/public/assets/characters/PokemonTrainer.png';
+import Robin from '/public/assets/characters/Robin.png';
+import Lucina from '/public/assets/characters/Lucina.png';
+import Chrom from '/public/assets/characters/Chrom.png';
+import Roy from '/public/assets/characters/Roy.png';
+import Rosaline from '/public/assets/characters/Rosalina.png';
+import Byleth from '/public/assets/characters/Byleth.png';
+import Pit from '/public/assets/characters/Pit.png';
+import DarkPit from '/public/assets/characters/DarkPit.png';
+import Palutena from '/public/assets/characters/Palutena.png';
+import Olimar from '/public/assets/characters/Olimar.png';
+import Simon from '/public/assets/characters/Simon.png';
+import Richter from '/public/assets/characters/Richter.png';
+import KingKRool from '/public/assets/characters/KingKRool.png';
+import Isabelle from '/public/assets/characters/Isabelle.png';
+import Incineroar from '/public/assets/characters/Incineroar.png';
+import Joker from '/public/assets/characters/Joker.png';
+import Hero from '/public/assets/characters/Hero.png';
+import BanjoKazooie from '/public/assets/characters/BanjoKazooie.png';
+import Terry from '/public/assets/characters/Terry.png';
+import Mii from '/public/assets/characters/Mii.png';
+import MinMin from '/public/assets/characters/MinMin.png';
+import Kazuya from '/public/assets/characters/Kazuya.png';
+import Sora from '/public/assets/characters/Sora.png';
+import Sonic from '/public/assets/characters/Sonic.png';
+import Mewtwo from '/public/assets/characters/Mewtwo.png';
+import Greninja from '/public/assets/characters/Greninja.png';
+import Lucario from '/public/assets/characters/Lucario.png';
+import Megaman from '/public/assets/characters/Megaman.png';
+import WiiFitTrainer from '/public/assets/characters/WiiFitTrainer.png';
+import ROB from '/public/assets/characters/ROB.png';
+import DuckHunt from '/public/assets/characters/DuckHunt.png';
+import MrGameAndWatch from '/public/assets/characters/MrGameAndWatch.png';
+import Mac from '/public/assets/characters/Mac.png';
+import Wario from '/public/assets/characters/Wario.png';
+import PacMan from '/public/assets/characters/PacMan.png';
+import Shulk from '/public/assets/characters/Shulk.png';
+import Cloud from '/public/assets/characters/Cloud.png';
+import Bayonetta from '/public/assets/characters/Bayonetta.png';
+import Corrin from '/public/assets/characters/Corrin.png';
+import Ken from '/public/assets/characters/Ken.png';
+import Ryu from '/public/assets/characters/Ryu.png';
+import Daisy from '/public/assets/characters/Daisy.png';
+import Peach from '/public/assets/characters/Peach.png';
+import Inkling from '/public/assets/characters/Inkling.png';
+import Snake from '/public/assets/characters/Snake.png';
+import Pyra from '/public/assets/characters/Pyra.png';
+import Mythra from '/public/assets/characters/Mythra.png';
+import Steve from '/public/assets/characters/Steve.png';
+import Sephiroth from '/public/assets/characters/Sephiroth.png';
+import Marth from '/public/assets/characters/Marth.png';
+import Bowser from '/public/assets/characters/Bowser.png';
+import BowserJr from '/public/assets/characters/BowserJr.png';
+
+
+export default function Home() {
+    return (
+      <main>
+        <Image src={Mario} width={200} height={200} alt="mario"></Image>
+      </main>
+    );
+  }
+  
