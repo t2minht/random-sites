42fb0f00fc10c49e8e5f6f0084f73e3904b8526b
t2minht <tuongtran0240@gmail.com>
2024-09-06 05:12:58 -0500

    base page created

---
+++ src/app/innerpage/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      inner page
+    </main>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Welcome to home page, there is nothing here</h1>
+    </main>
+  );
+}
