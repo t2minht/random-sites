6a8b82828b0c3cd775d4371ad8de9cc0d8389d20
t2minht <tuongtran0240@gmail.com>
2024-09-06 22:04:05 -0500

    fixed app's page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Welcome to home page, there is nothing here</h1>
     </main>
   );
